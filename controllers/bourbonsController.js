c27c9381eddd68ac22ea969dafb6f02a1546ab81
Alvin Johnson <ALvinjohnson144@gmail.com>
2024-02-16 13:57:57 -0500

    bourbonsID route added

--- controllers/bourbonsController.js
+++ controllers/bourbonsController.js
@@ -7,4 +7,21 @@ bourbons.get("/", (req, res) => {
     res.status(200).json({ message: "Bourbons Home Page" })
 })
 
+
+bourbons.get("/:bourbonsID", (req, res) => {
+    const bourbonsID = req.params.bourbonsID
+
+    if (Number(bourbonsID)) {
+        res.status(200).json({ message: bourbonsID })
+
+    }
+    else {
+        res.status(404).json({
+            error: "id must be a numeric value"
+        })
+    }
+
+})
+
 module.exports = bourbons
+
