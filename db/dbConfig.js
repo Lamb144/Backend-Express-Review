7d089ec8e4dd7bd1e758ac8b5950abac623898a5
alvinjohnson <alvinjohnson144@gmail.com>
2024-02-22 11:48:30 -0500

    pwd

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -8,7 +8,8 @@ const cn = {
     host: process.env.PG_HOST,
     user: process.env.PG_USER,
     port: process.env.PG_PORT,
-    database: process.env.PG_DATABASE
+    database: process.env.PG_DATABASE,
+    password: process.env.PG_PASSWORD
 }
 
 const db = pgp(cn)
